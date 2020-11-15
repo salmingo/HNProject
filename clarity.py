8cb10817b4bfb1664c4447d8960f5731c5cd2a50
salmingo <lxm@nao.cas.cn>
2020-11-15 23:27:54 +0800

    2020-11-14
    update

--- clarity.py
+++ clarity.py
@@ -97,9 +97,9 @@ class ReadLog:
                     m0      = float(tokens[8]);
                     k       = float(tokens[9]);
                     if m0 > 0.0 or k > 0.0:
-                        # 仪器星等=12, 调制
-                        m0 = m0 + k * 12.0;
-                        if (m0 <= 10.0):
+                        # 仪器星等=10, 调制
+                        m0 = 10.0 - (m0 + k * 10.0);
+                        if (m0 > 0.0):
                             item = SourceLogItem(airmass, m0);
                             self.items.append(item);
                             if (alt < altmin):
@@ -122,13 +122,15 @@ class ReadLog:
         if n < 5:
             return [False, 0.0];
         
-        mid = int(n / 2);
         samples.sort(key = lambda SourceLogItem: SourceLogItem.m0);
-        if n % 2 == 1:
-            m0 = samples[mid].m0;
-        else:
-            m0 = (samples[mid].m0 + samples[mid + 1].m0) * 0.5;
-        return [True, m0];
+        return [True, samples[1].m0];
+
+#         mid = int(n / 2);
+#         if n % 2 == 1:
+#             m0 = samples[mid].m0;
+#         else:
+#             m0 = (samples[mid].m0 + samples[mid + 1].m0) * 0.5;
+#         return [True, m0];
     
     def ExecSample(self):
         # 采样, 抽取同一大气质量的m0中值
@@ -177,24 +179,29 @@ class ReadLog:
         self.k  = p[0];
         self.c0 = p[1];
 #         print (p);
-        # 输出拟合结果
-        clarity = open('clarity-'+self.dateStr+".txt", "w");
-        clarity.write("%.3f    %.3f\n" % (self.c0, self.k));
-        clarity.write("--------------------------------\n");
-        for item in self.samples:
-            clarity.write("%4.1f  %4.1f\n" % (item.airmass, item.m0));
-        clarity.close();
-        # 画图
-        plt.clf();
-        plt.plot(airmass, m0, '.', airmass, np.polyval(p, x), '-');
-        plt.plot(airmass, np.polyval(p, x), '-');
-        plt.title(self.dateStr);
-        plt.xlabel('Airmass');
-        plt.ylabel('Magnitude');
-        plt.text(np.mean(airmass), np.mean(m0), 'm=%.3f%+.3fx' % (self.c0, self.k));
-        plt.show(block = False);
-        # 将绘图存储为PNG图像
-        plt.savefig("clarity-"+self.dateStr+".png");
+        
+        if self.k > 0.0 and self.k < 3.0:
+            # 输出拟合结果
+            clarity = open('clarity-'+self.dateStr+".txt", "w");
+            clarity.write("%.3f    %.3f\n" % (self.c0, self.k));
+            clarity.write("--------------------------------\n");
+            for item in self.samples:
+                clarity.write("%4.1f  %4.1f\n" % (item.airmass, item.m0));
+            clarity.close();
+            # 画图
+            plt.clf();
+            plt.plot(airmass, m0, '.', airmass, np.polyval(p, x), '-');
+            plt.plot(airmass, np.polyval(p, x), '-');
+            plt.title(self.dateStr);
+            plt.xlabel('Airmass');
+            plt.ylabel('Magnitude');
+            plt.text(np.mean(airmass), np.mean(m0), 'm=%.3f%+.3fx' % (self.c0, self.k));
+            plt.show(block = False);
+            # 将绘图存储为PNG图像
+            plt.savefig("clarity-"+self.dateStr+".png");
+            return True;
+        
+        return False;
 
 # end class ReadLog:
 
@@ -233,15 +240,32 @@ if __name__ == '__main__':
 
     plt.figure(figsize=(5,5));
 
+    dateStr = [];
+    extZ = [];
     clarity = open("clarity.txt", "w");        
     # 遍历原始文件
     for i in range(count):
         print (sourceList[i]);
         log_src = ReadLog(sourceList[i]);
         if log_src.Count() > 0 and log_src.ExecSample() >= 5:
-            log_src.DoFit();
-            clarity.write("%s   %7.3f\n" % (log_src.dateStr, log_src.k));
+            if log_src.DoFit() == True:
+                c0 = log_src.c0 + log_src.k + 0.15; # 0.15: 实际数据受外界因素影响质量较差, 拟合结果失真. +0.15调制使其接近真值
+                if c0 > 0.0:
+                    dateStr.append(log_src.dateStr);
+                    extZ.append(c0);
+                    clarity.write("%s  %4.1f  %7.3f\n" % (log_src.dateStr, c0, log_src.k));
     
     clarity.close();
+    
+    # 绘画
+    plt.clf();
+    plt.plot(dateStr, extZ, '.');
+    plt.title("clarity");
+    plt.xlabel('date');
+    plt.ylabel('AirExtinction');
+#     plt.show(block = False);
+    # 将绘图存储为PNG图像
+    plt.savefig("clarity.png");
+
     plt.show(block=True);
     
